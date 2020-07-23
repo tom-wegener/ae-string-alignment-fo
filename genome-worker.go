1a2c7ebc9b5338df8f1a9f926c710e61bcc68a2c
Tom Wegener <wegener.tom@outlook.com>
2020-07-23 19:22:40 +0200

    switch libs, add some more mutating-functions

--- genome-worker.go
+++ genome-worker.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"crypto/rand"
-	"math/big"
+	"math/rand"
 )
 
 //Child is a child or genome with its fitness
 type Child struct {
 	flow    [][]int64
+	demand  []int64
 	storage []int64
 	fitness int64
 }
@@ -17,34 +17,118 @@ func (x *Child) toParent(c *Child) {
 	c.fitness = x.fitness
 }
 
-// mutateDumb: mutate the genome on a random place
-func (x *Child) mutateDumb() {
+// mutateDumb: mutate the genome on all places but random place
+//  - mutates all
+func (x *Child) mutateDumb(c *Child) {
+	c.flow = x.flow
+	c.fitness = x.fitness
+	c.storage = x.storage
+	c.demand = x.demand
+	localStorage := x.demand
 
 	for i, row := range x.flow {
 		for j := range row {
-			randomInt, err := rand.Int(rand.Reader, big.NewInt(10))
-			errFunc(err)
-			if x.flow[i][j] != randomInt.Int64() {
-				x.flow[i][j] = randomInt.Int64()
-			}
+			randomInt := rand.Int63n(10)
+			localStorage[i] = localStorage[i] - randomInt
+			localStorage[j] = localStorage[j] + randomInt
+			c.flow[i][j] = randomInt
 		}
 	}
 }
 
-//
-func (x *Child) findNeighbour(c *Child) {
+// find a new Neighbour but still relatively random
+//  - only mutates one
+func (x *Child) findNeighbourZero(c *Child) {
 	c.flow = x.flow
 	c.fitness = x.fitness
-	randomI, err := rand.Int(rand.Reader, big.NewInt(10))
-	errFunc(err)
-	randomI64 := randomI.Int64()
-	randomJ, err := rand.Int(rand.Reader, big.NewInt(9))
-	errFunc(err)
-	randomJ64 := randomJ.Int64()
-	randomInt, err := rand.Int(rand.Reader, big.NewInt(10))
-	if c.flow[randomI64][randomJ64] != randomInt.Int64() {
-		c.flow[randomI64][randomJ64] = randomInt.Int64()
+	c.storage = x.storage
+	c.demand = x.demand
+	localStorage := x.demand
+
+	randomI := rand.Int63n(10)
+	randomJ := rand.Int63n(9)
+	randomInt := rand.Int63n(10)
+
+	c.flow[randomI][randomJ] = randomInt
+
+	for i := randomI; i >= 0; i-- {
+		for j := randomJ; j >= 0; j-- {
+			localStorage[i] = localStorage[i] - x.flow[i][j]
+			localStorage[j] = localStorage[j] + x.flow[i][j]
+			c.flow[i][j] = randomInt
+		}
 	}
+	c.storage = localStorage
+	if c.flow[randomI][randomJ] != randomInt {
+
+	}
+
+}
+
+// find a new Neighbour
+//  - mutates one randomly
+//  - corrects multiple after the mutated
+//  - recognize demand/storage
+func (x *Child) findNeighbourOne(c *Child) {
+	c.flow = x.flow
+	c.fitness = x.fitness
+	c.storage = x.storage
+	c.demand = x.demand
+	localStorage := x.demand
+
+	randomI := rand.Intn(10)
+
+	randomJ := rand.Intn(9)
+
+	for i := randomI; i >= 0; i-- {
+		for j := randomJ; j >= 0; j-- {
+			if localStorage[i] > 0 {
+				randomInt := rand.Int63n(localStorage[i])
+				localStorage[i] = localStorage[i] - randomInt
+				localStorage[j] = localStorage[j] + randomInt
+				c.flow[i][j] = randomInt
+			}
+		}
+	}
+	c.storage = localStorage
+	return
+}
+
+// find a new Neighbour
+//  - only mutates one
+//  - recognize demand/storage
+//  - only mutate existing edges
+func (x *Child) findNeighbourTwo(c *Child, network [][]bool) {
+	c.flow = x.flow
+	c.fitness = x.fitness
+	c.storage = x.storage
+	c.demand = x.demand
+	localStorage := x.demand
+
+	var edges [][]int
+	for i, row := range network {
+		for j, cell := range row {
+			if cell {
+				edge := []int{i, j}
+				edges = append(edges, edge)
+			}
+		}
+	}
+
+	randomEdge := rand.Intn(len(edges))
+
+	for i := edges[randomEdge][0]; i >= 0; i-- {
+		for j := edges[randomEdge][1]; j >= 0; j-- {
+			if localStorage[i] > 0 {
+				randomInt := rand.Int63n(localStorage[i])
+				localStorage[i] = localStorage[i] - randomInt
+				localStorage[j] = localStorage[j] + randomInt
+				c.flow[i][j] = randomInt
+			}
+		}
+	}
+	c.storage = localStorage
+	return
 
 }
 
@@ -56,8 +140,8 @@ func (x *Child) initiateFlowZero(verticesCount int) {
 	for i := 0; i < verticesCount; i++ {
 		var flowX []int64
 		for j := 0; j < (verticesCount - 1); j++ {
-			randomInt, _ := rand.Int(rand.Reader, big.NewInt(10))
-			flowX = append(flowX, randomInt.Int64())
+			randomInt := rand.Int63n(10)
+			flowX = append(flowX, randomInt)
 		}
 		flowAll = append(flowAll, flowX)
 	}
@@ -65,51 +149,45 @@ func (x *Child) initiateFlowZero(verticesCount int) {
 	return
 }
 
-// initiateFlowSmarter: this function generates a first flow which only uses existing vertices
+// initiateFlowSmarter: this function generates a first flow
+//  - only uses existing vertices
 func (x *Child) initiateFlowOne(verticesCount int, customerDemand []int64, network [][]bool) {
-	var flowAll [][]int64
 
 	for i := verticesCount; i > 0; i-- {
 		var flowX []int64
 		for j := (verticesCount - 1); j > 0; j-- {
 			if network[i][j] {
-				randomInt, _ := rand.Int(rand.Reader, big.NewInt(10))
-				flowX = append(flowX, randomInt.Int64())
+				randomInt := rand.Int63n(10)
+				flowX = append(flowX, randomInt)
 			} else {
 				flowX = append(flowX, 0)
 			}
 		}
-		flowAll = append(flowAll, flowX)
+		x.flow = append(x.flow, flowX)
 	}
-	x.flow = flowAll
 	return
 }
 
-// initiateFlowSmarter: this function generates a first flow which only uses existing vertices
-func (x *Child) initiateFlowTwo(verticesCount int, customerDemand []int64, network [][]bool, sourceCapacity int64) {
-	var flowAll [][]int64
+// initiateFlowTwo: this function generates a first flow
+//  - only uses existing vertices
+//  - recognizes the demand
+func (x *Child) initiateFlowTwo(verticesCount int, customerDemand []int64, network [][]bool) {
 
-	// The demand could be seen as negative storage capacity
-	// Also the last node is the source
-	for i := range customerDemand {
-		x.storage = append(x.storage, -1*customerDemand[i])
-	}
-	x.storage = append(x.storage, sourceCapacity)
+	localStorage := x.demand
 
 	for i := (verticesCount - 1); i >= 0; i-- {
 		var flowX []int64
 		for j := (verticesCount - 2); j >= 0; j-- {
-			if network[i][j] && x.storage[i] > 0 {
-				randomInt, _ := rand.Int(rand.Reader, big.NewInt(x.storage[i]))
-				x.storage[i] = x.storage[i] - randomInt.Int64()
-				x.storage[j] = x.storage[j] + randomInt.Int64()
-				flowX = append(flowX, randomInt.Int64())
+			if network[i][j] && localStorage[i] > 0 {
+				randomInt := rand.Int63n(localStorage[i])
+				localStorage[i] = localStorage[i] - randomInt
+				localStorage[j] = localStorage[j] + randomInt
+				flowX = append(flowX, randomInt)
 			} else {
 				flowX = append(flowX, 0)
 			}
 		}
-		flowAll = append(flowAll, flowX)
+		x.flow = append(x.flow, flowX)
 	}
-	x.flow = flowAll
 	return
 }
