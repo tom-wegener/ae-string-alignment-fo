cdca55538cebe871fc36ad97a9d14b11f20cc18f
Tom Wegener <wegener.tom@outlook.com>
2020-07-23 17:38:46 +0200

    add storage and better initation

--- genome-worker.go
+++ genome-worker.go
@@ -8,6 +8,7 @@ import (
 //Child is a child or genome with its fitness
 type Child struct {
 	flow    [][]int64
+	storage []int64
 	fitness int64
 }
 
@@ -48,7 +49,7 @@ func (x *Child) findNeighbour(c *Child) {
 }
 
 // initiateFlowDumb: this function generates a first very random genome
-func (x *Child) initiateFlowDumb(verticesCount int) {
+func (x *Child) initiateFlowZero(verticesCount int) {
 
 	var flowAll [][]int64
 
@@ -65,14 +66,43 @@ func (x *Child) initiateFlowDumb(verticesCount int) {
 }
 
 // initiateFlowSmarter: this function generates a first flow which only uses existing vertices
-func (x *Child) initiateFlowSmarter(verticesCount int, customerDemand []int64, network [][]bool) {
+func (x *Child) initiateFlowOne(verticesCount int, customerDemand []int64, network [][]bool) {
 	var flowAll [][]int64
 
-	for i := 0; i < verticesCount; i++ {
+	for i := verticesCount; i > 0; i-- {
 		var flowX []int64
-		for j := 0; j < (verticesCount - 1); j++ {
+		for j := (verticesCount - 1); j > 0; j-- {
 			if network[i][j] {
 				randomInt, _ := rand.Int(rand.Reader, big.NewInt(10))
+				flowX = append(flowX, randomInt.Int64())
+			} else {
+				flowX = append(flowX, 0)
+			}
+		}
+		flowAll = append(flowAll, flowX)
+	}
+	x.flow = flowAll
+	return
+}
+
+// initiateFlowSmarter: this function generates a first flow which only uses existing vertices
+func (x *Child) initiateFlowTwo(verticesCount int, customerDemand []int64, network [][]bool, sourceCapacity int64) {
+	var flowAll [][]int64
+
+	// The demand could be seen as negative storage capacity
+	// Also the last node is the source
+	for i := range customerDemand {
+		x.storage = append(x.storage, -1*customerDemand[i])
+	}
+	x.storage = append(x.storage, sourceCapacity)
+
+	for i := (verticesCount - 1); i >= 0; i-- {
+		var flowX []int64
+		for j := (verticesCount - 2); j >= 0; j-- {
+			if network[i][j] && x.storage[i] > 0 {
+				randomInt, _ := rand.Int(rand.Reader, big.NewInt(x.storage[i]))
+				x.storage[i] = x.storage[i] - randomInt.Int64()
+				x.storage[j] = x.storage[j] + randomInt.Int64()
 				flowX = append(flowX, randomInt.Int64())
 			} else {
 				flowX = append(flowX, 0)
