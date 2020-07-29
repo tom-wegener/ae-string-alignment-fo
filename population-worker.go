d05923a9b57547514e15e814942b99adf028025e
Tom Wegener <wegener.tom@outlook.com>
2020-07-29 17:50:15 +0200

    better evolution & crossover & selection

--- population-worker.go
+++ population-worker.go
@@ -6,7 +6,7 @@ import (
 	"sort"
 )
 
-// ByFitness implements sort-Interface for the Childs based on their fitness
+// ByFitness implements sort-Interface for []Child based on their fitness
 type ByFitness []Child
 
 func (a ByFitness) Len() int           { return len(a) }
@@ -26,41 +26,83 @@ func populate(network [][]bool, verticesCount int, demand []int64, costsA, costs
 	return
 }
 
-func ranking(population []Child) (pool []Child) {
+func selectionRanking(population []Child) (pool []Child) {
+	pool = make([]Child, 0)
 	sort.Sort(ByFitness(population))
-	numOfRanks := len(population)
-	for i, individuum := range population {
-		rank := i / 2 / numOfRanks * (1 - (i / numOfRanks))
+	numOfRanks := float64(len(population))
+	var i float64
+	for i = 0; i < numOfRanks; i++ {
+		rank := (2 / numOfRanks) * (1 - (i / (numOfRanks - 1))) * numOfRanks
 
-		for j := numOfRanks; j > rank; j-- {
-			pool = append(pool, individuum)
+		for j := float64(0); j < rank; j++ {
+			pool = append(pool, population[int(i)])
 		}
 	}
 	return
 }
 
-func selection(population []Child, costA, costB, costC [][]int64) (pool []Child) {
+func selectionTurnier(population []Child) (pool []Child) {
+	pool = population
+	gegner := 3
+
+	for i := 0; i < len(pool); i++ {
+		for j := 0; j < gegner; j++ {
+			rI := rand.Intn(len(pool))
+			if pool[i].fitness < pool[rI].fitness {
+				pool[rI] = pool[i]
+			}
+		}
+	}
+	return
+}
+func evolution(population []Child, costA, costB, costC [][]int64, network [][]bool) (pool []Child) {
+	pool = make([]Child, 0)
 	for i := 0; i < cfg.PopulationSize; i++ {
+		var x Child
+		var c Child
 		r1 := rand.Intn(len(population))
 		r2 := rand.Intn(len(population))
-		x := population[r1]
-		c := population[r2]
-		c.twoPointCrossover(&x)
-		c.randomMutate()
+		x = population[r1]
+		c = population[r2]
+		c.onePointCrossover(&x)
+
+		c.betterMutate(network)
+
 		c.costCalculator(costA, costB, costC)
 		if c.fitness < 0 {
-			printChild(x, 200)
-			for i, row := range x.flow {
+			println(c.fitness)
+			for i, row := range c.flow {
+				print(c.storage[i], ", ")
 				for j := range row {
-					print(x.flow[i][j], ",")
+					print(c.flow[i][j], ", ")
 				}
 				println()
 			}
 			os.Exit(0)
+
 		}
 		pool = append(pool, c)
 	}
 	return
+}
+
+func (x *Child) onePointCrossover(c *Child) {
+
+	childLen := len(x.flow) * len(x.flow[0])
+	crossoverPoint := rand.Intn(childLen)
+
+	n := 0
+	for i, row := range x.flow {
+		for j := range row {
+			if crossoverPoint < n {
+				tmp := c.flow[i][j]
+				c.flow[i][j] = x.flow[i][j]
+				x.flow[i][j] = tmp
+			}
+			n++
+		}
+	}
+
 }
 
 func (x *Child) twoPointCrossover(c *Child) {
@@ -78,7 +120,11 @@ func (x *Child) twoPointCrossover(c *Child) {
 	for i, row := range x.flow {
 		for j := range row {
 			if crossoverPointOne < n && n < crossoverPointTwo {
+
+				tmp := c.flow[i][j] - x.flow[i][j]
 				x.flow[i][j] = c.flow[i][j]
+				x.storage[i] = x.storage[i] + tmp
+				x.storage[i] = x.storage[j] - tmp
 			}
 			n++
 		}
@@ -86,14 +132,15 @@ func (x *Child) twoPointCrossover(c *Child) {
 
 }
 
-func (x *Child) randomMutate() {
+func (x *Child) randomMutate(network [][]bool) {
 	for i, row := range x.flow {
 		for j := range row {
 			if rand.Float64() < cfg.MutationRate {
-				if x.storage[i]+x.flow[i][j] > 0 && x.flow[i][j] > 0 {
+				storeI := x.storage[i] + x.flow[i][j]
+				if storeI > 0 && network[i][j] {
 					x.storage[i] = x.storage[i] + x.flow[i][j]
 					x.storage[j] = x.storage[j] - x.flow[i][j]
-					x.flow[i][j] = rand.Int63n(x.storage[i])
+					x.flow[i][j] = rand.Int63n(storeI)
 					x.storage[i] = x.storage[i] - x.flow[i][j]
 					x.storage[j] = x.storage[j] + x.flow[i][j]
 				}
@@ -101,4 +148,38 @@ func (x *Child) randomMutate() {
 			}
 		}
 	}
+}
+
+func (x *Child) betterMutate(network [][]bool) {
+	localStorage := x.demand
+
+	var edges [][]int
+	for i, row := range network {
+		for j, cell := range row {
+			if cell {
+				edge := []int{i, j}
+				edges = append(edges, edge)
+			}
+		}
+	}
+	for i := range edges {
+		if rand.Float64() < cfg.MutationRate {
+
+			for i := edges[i][0]; i >= 0; i-- {
+				for j := edges[i][1]; j >= 0; j-- {
+					if localStorage[i] > 0 {
+						randomInt := rand.Int63n(localStorage[i])
+						localStorage[i] = localStorage[i] - randomInt
+						localStorage[j] = localStorage[j] + randomInt
+						x.flow[i][j] = randomInt
+					}
+				}
+			}
+			x.storage = localStorage
+		}
+
+	}
+
+	return
+
 }
