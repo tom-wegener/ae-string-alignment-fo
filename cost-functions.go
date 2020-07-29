4321d528335d6f2383c633348502c1950fa7ff23
Tom Wegener <wegener.tom@outlook.com>
2020-07-29 17:48:40 +0200

    better cost-function

--- cost-functions.go
+++ cost-functions.go
@@ -1,40 +1,46 @@
 package main
 
-import "math"
-
 // x[i][j] is the flow for the between the vertices i and j
 // The functions get the three cost-matrices (variable a, varioable b, fixed c) and the flow-matrix and calculate the overall cost of the flow-matrix.
 // We can use them as to calculate our overall fitness
 
 func (x *Child) costCalculator(a, b, c [][]int64) {
-	function := 0
-
-	if function == 0 {
-		x.costEstimatorZero(a, b, c)
-	}
+	x.costEstimatorZero(a, b, c)
 }
 
 func (x *Child) costEstimatorZero(a, b, c [][]int64) {
-	var costsX int64
-	var costs int64
+	var costs int64 = 0
 	for i, row := range a {
+		var costsX int64
 		for j := range row {
 			costsX = costsX + costEstimatorZeroEdge(a[i][j], b[i][j], c[i][j], x.flow[i][j])
 		}
 		costs = costs + costsX
 	}
-	var punishment float64
+
+	var punishment int64 = 0
 	for _, store := range x.storage {
-		storeFloat := float64(store)
-		punishment = punishment + math.Abs(storeFloat)
+		if store != 0 {
+			punishment = punishment + abs(store)
+		}
 	}
-	punishmentInt := int64(punishment) * 100000
-	x.fitness = costs + punishmentInt
+	costs = costs + punishment*100000
+	x.fitness = costs
 	return
 }
 
 func costEstimatorZeroEdge(aij, bij, cij int64, xij int64) (costs int64) {
-	costs = -aij*xij*xij + bij*xij + cij
+	costs = aij*xij*xij + bij*xij + cij
+	if costs < 0 {
+		println(-aij, xij, bij, xij, cij)
+	}
 	return
 
+}
+
+func abs(i int64) int64 {
+	if i < 0 {
+		return -i
+	}
+	return i
 }
