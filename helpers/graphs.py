4ddabff9cfde90f36455c526d30e6ab87bb8bf56
Tom Wegener <wegener.tom@outlook.com>
2020-07-31 16:29:45 +0200

    new stats

--- helpers/graphs.py
+++ helpers/graphs.py
@@ -6,14 +6,35 @@ import csv
 
 
 def main():
-    data = []
+    data0 = []
+    data1 = []
+    data2 = []
+    data3 = []
     time = []
-    fileName = "2000pop_2000gen_5geg_0.05druck"
+    fileName = "80pop_2000gen_2geg_0.05druck_opc"
     with open(fileName + ".csv") as csvfile:
         csvreader = csv.reader(csvfile)
         for row in csvreader:
             time.append(int(row[0]))
-            data.append(int(row[1]))
+            data0.append(int(row[1]))
+
+    fileName = "80pop_2000gen_3geg_0.05druck_opc"
+    with open(fileName + ".csv") as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            data1.append(int(row[1]))
+
+    fileName = "80pop_2000gen_5geg_0.05druck_opc"
+    with open(fileName + ".csv") as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            data2.append(int(row[1]))
+
+    fileName = "80pop_2000gen_7geg_0.05druck_opc"
+    with open(fileName + ".csv") as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            data3.append(int(row[1]))
 
     fig_size = plt.rcParams["figure.figsize"]
     fig_size[0] = 14
@@ -22,13 +43,21 @@ def main():
 
     y_formatter = ScalarFormatter(useOffset=False)
     plt.axes().yaxis.set_major_formatter(y_formatter)
-    plt.plot(time, data)
+    
+
+
+    plt.plot(time, data0, 'r', label='2 Gegner')
+    plt.plot(time, data1, 'b',label='3 Gegner')
+    plt.plot(time, data2, 'g',label='5 Gegner')
+    plt.plot(time, data3,'y',label='7 Gegner')
     plt.xlabel("Generation")
     plt.ylabel("Güte-Wert")
-    plt.ylim(0, max(data)+1000)
+    maxAll = [max(data1), max(data0)]
+    plt.ylim(0, max(maxAll))
     plt.xlim(0, max(time))
+    plt.legend(loc="upper right")
 
-    plt.savefig(fileName+".png", bbox_inches="tight")
+    plt.savefig("opc_tpc_2.png", bbox_inches="tight")
 
 
 main()
